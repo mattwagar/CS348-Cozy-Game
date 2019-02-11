a892cb55d66561991dc99cff328ba9cbad3bd279
Matthew Wagar <mrw77@drexel.edu>
2019-02-10 20:02:02 -0500

    Basic keycode functionality

---
+++ Cozy Game/Assets/Scripts/KeySprite.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class KeySprite : MonoBehaviour
+{
+    public KeyCode keyCode;
+    
+    private SpriteRenderer spriteRenderer;
+
+    private bool keyPressed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(keyCode))
+        {
+            keyPressed = true;
+        }  
+        if (Input.GetKeyUp(keyCode)){
+            keyPressed = false;
+        }
+
+        if(keyPressed){
+            spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, 1.0f); 
+        }  else {
+            spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, 0.0f); 
+        }
+    }
+
+    public void InitializeKey()
+    {
+        gameObject.SetActive(true);
+    }
+}
---
+++ Cozy Game/Assets/Scripts/KeySpriteManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeySpriteManager : MonoBehaviour
+{
+
+    public KeySprite[] keySprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // GetComponentInChildren<KeySprite>();
+        for(int i = 0; i < keySprite.Length; i++)
+        {
+            keySprite[i].InitializeKey();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
