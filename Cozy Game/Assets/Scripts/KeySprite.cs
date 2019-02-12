c3fe08cf02fac3f79c451f35f9d6e69c1a872b17
Matthew Napolillo <msn49@drexel.edu>
2019-02-12 14:40:17 -0500

    Audio and particles should now all be working as intended!

--- Cozy Game/Assets/Scripts/KeySprite.cs
+++ Cozy Game/Assets/Scripts/KeySprite.cs
@@ -15,33 +15,67 @@ public class KeySprite : MonoBehaviour
 
     private bool keyPressed;
     
+    private float volumeChangeSpeed = 0.2f;
+
+    private Coroutine audioRoutine;
+    
     // Start is called before the first frame update
     void Start()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        audioSource = GetComponent<AudioSource>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(keyCode))
+        if(particleEffect != null)
         {
-            keyPressed = true;
-        }  
-        if (Input.GetKeyUp(keyCode)){
-            keyPressed = false;
+            audioSource = GetComponent<AudioSource>();
+            StartCoroutine(EffectsLoop());
         }
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
-        if(keyPressed){
-            //spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, 1.0f);
+    private IEnumerator EffectsLoop()
+    {
+        while(true)
+        {
+            yield return new WaitUntil(() => Input.GetKeyDown(keyCode));
+            Debug.Log("KeyPressed");
+            particleEffect.Stop();
             particleEffect.Play();
-            //Might add some stuff here to modify volume on keypresses to be more dynamic
+            //spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, 1.0f);
+            audioSource.volume = 1;
             audioSource.Play();
-        }  else {
-            //spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, 0.0f);
+            yield return new WaitUntil(() => Input.GetKeyUp(keyCode));
+            Debug.Log("KeyReleased");
+            if(audioRoutine != null)
+                StopCoroutine(audioRoutine);
+            audioRoutine = StartCoroutine(AudioRoutine());
         }
     }
+
+    private IEnumerator AudioRoutine()
+    {
+        float currentVolume = 1f;
+        while(currentVolume >= 0)
+        {
+            currentVolume -= Time.deltaTime * volumeChangeSpeed;
+            audioSource.volume = currentVolume;
+            yield return null;
+        }
+        audioSource.volume = 0;
+        audioSource.Stop();
+    }
+
+    // // Update is called once per frame
+    // void FixedUpdate()
+    // {
+    //     if ()
+    //     {
+    //         // Debug.Log("KeyPressed");
+    //         keyPressed = true;
+    //     }  
+    //     if (Input.GetKeyUp(keyCode))
+    //     {
+    //         // Debug.Log("KeyReleased");
+    //         keyPressed = false;
+    //     }
+    // }
 
     public void InitializeKey()
     {
